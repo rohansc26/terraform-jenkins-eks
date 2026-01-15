77c4000fdfa83804ad0be5552d39f94f4c169643
rohan <rohansc26@gmail.com>
2026-01-15 12:39:24 +0530

    lattest modified code for EKS

--- EKS/backend.tf
+++ EKS/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "cicd-terraform-eks-2378"
-    key    = "eks/terraform.tfstate"
-    region = "ap-south-1"
+    bucket         = "cicd-terraform-eks-2378"
+    key            = "eks/terraform.tfstate"
+    region         = "ap-south-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
   }
